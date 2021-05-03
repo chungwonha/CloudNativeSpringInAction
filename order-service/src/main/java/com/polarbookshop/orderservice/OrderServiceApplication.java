e6bdd4b96e4e12a92d4e7804e3f31d0a8cd97e6e
chungha@yahoo.com <chungha@yahoo.com>
2021-05-02 23:14:52 -0400

    quick test

--- order-service/src/main/java/com/polarbookshop/orderservice/OrderServiceApplication.java
+++ order-service/src/main/java/com/polarbookshop/orderservice/OrderServiceApplication.java
@@ -8,7 +8,7 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 @SpringBootApplication
 @EnableConfigurationProperties(BookClientProperties.class)
 public class OrderServiceApplication {
-
+	/*test*/
 	public static void main(String[] args) {
 		SpringApplication.run(OrderServiceApplication.class, args);
 	}
